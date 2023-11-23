c0f13102d399fdc737660b43e45040344733d53e
lubos zeman <lubos.zeman@redrobot.cz>
2023-11-23 15:58:34 +0100

    project creation

---
+++ src/main/java/com/example/ocgBackand/OcgBackandApplication.java
@@ -0,0 +1,13 @@
+package com.example.ocgBackand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OcgBackandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OcgBackandApplication.class, args);
+	}
+
+}
