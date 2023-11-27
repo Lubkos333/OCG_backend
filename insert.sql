c77162241a1fd3e7b907b52fae6dac6089ffc4ee
Szii <79159773+Szii@users.noreply.github.com>
2023-11-27 19:37:35 +0100

    Update insert.sql

--- insert.sql
+++ insert.sql
@@ -4,9 +4,9 @@ INSERT INTO cardgame.achievements (`achievement_name`,`achievemen_desc`,`achieve
 INSERT INTO cardgame.cards (`card_name`, `card_desc`, `card_value`, `card_attack`,`card_image`) VALUES ("Test_card", "This card does stuff",1,1,"C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/card_placeholder.jpg");
 INSERT INTO cardgame.cards (`card_name`, `card_desc`, `card_value`, `card_attack`) VALUES ("Another_test_card", "This card does stuff too",2,2);
 
-INSERT INTO cardgame.users (`user_name`, `user_password`) VALUES ("Franta Lala", "1234");
-INSERT INTO cardgame.users (`user_name`, `user_password`) VALUES ("John Doe", "1111");
-INSERT INTO cardgame.users (`user_name`, `user_password`) VALUES ("PussyKiller3000", "0000");
+INSERT INTO cardgame.users (`user_name`, `user_password`,user_email) VALUES ("Franta Lala", "1234","franta@seznam.cz");
+INSERT INTO cardgame.users (`user_name`, `user_password`) VALUES ("John Doe", "1111","doe@gmail.com"));
+INSERT INTO cardgame.users (`user_name`, `user_password`) VALUES ("PussyKiller3000", "0000","auticko@seznam.cz"));
 
 INSERT INTO cardgame.friends (`user_1_ref`, `user_2_ref`,`pending`) VALUES ("1", "2","0");
 INSERT INTO cardgame.friends (`user_1_ref`, `user_2_ref`,`pending`) VALUES ("2", "3","1");
