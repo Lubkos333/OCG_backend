c2ef1bb0902daa087b57b0e9a97d7dadcc2504b5
lubos zeman <lubos.zeman@redrobot.cz>
2023-11-29 21:51:35 +0100

    Edited sql files to make them nice and friendly for PostgresQL <3

--- src/main/resources/db/migration/afterMigrate.sql
+++ src/main/resources/db/migration/afterMigrate.sql
@@ -1,2 +1,25 @@
-INSERT INTO users(name)
-values ('Lubex');
+INSERT INTO achievements (achievement_name,achievemen_desc,achievemen_thresold) VALUES ('First Blood','Your mommy will be proud', 1);
+INSERT INTO achievements (achievement_name,achievemen_desc,achievemen_thresold) VALUES ('Second Blood','Your mommy will be very proud', 2);
+
+INSERT INTO cards (card_name, card_desc, card_value, card_attack,card_image) VALUES ('Test_card', 'This card does stuff',1,1,'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/card_placeholder.jpg');
+INSERT INTO cards (card_name, card_desc, card_value, card_attack) VALUES ('Another_test_card', 'This card does stuff too',2,2);
+
+INSERT INTO users (user_name, user_password, user_email) VALUES ('Franta Lala', '1234','franta@seznam.cz');
+INSERT INTO users (user_name, user_password, user_email) VALUES ('John Doe', '1111','doe@gmail.com');
+INSERT INTO users (user_name, user_password, user_email) VALUES ('PussyKiller3000', '0000','auticko@seznam.cz');
+
+INSERT INTO friends (user_1_ref, user_2_ref, pending) VALUES ('1', '2','0');
+INSERT INTO friends (user_1_ref, user_2_ref, pending) VALUES ('2', '3','1');
+
+INSERT INTO user_achievements (user_ref, achievement_ref) VALUES('1','1');
+INSERT INTO user_achievements (user_ref, achievement_ref) VALUES('1','2');
+
+INSERT INTO user_cards (user_ref, card_ref) VALUES('1','1');
+INSERT INTO user_cards (user_ref, card_ref) VALUES('1','2');
+INSERT INTO user_cards (user_ref, card_ref) VALUES('2','1');
+INSERT INTO user_cards (user_ref, card_ref) VALUES('2','2');
+INSERT INTO user_cards (user_ref, card_ref) VALUES('3','1');
+INSERT INTO user_cards (user_ref, card_ref) VALUES('3','2');
+
+
+
