4a2ae954f57024d0c76d4373f2c8ef0e644c6d15
brune <brune@DESKTOP-QSV8A7T.home>
2023-12-05 16:45:39 +0100

    create user entity

--- src/main/java/com/example/ocgBackend/persistence/model/User.java
+++ src/main/java/com/example/ocgBackend/persistence/model/User.java
@@ -14,7 +14,46 @@ import lombok.*;
 @NoArgsConstructor
 public class User extends IdEntity {
 
-    @Column(name = "name")
+    @Column(name = "user_name")
     private String name;
-
+    @Column(name = "user_password")
+    private String password;
+    @Column(name = "victory_count")
+    private int victoryCount;
+    @Column(name = "user_email")
+    private String email;
+    
+    public User(){}
+    
+    public void setName(String name){
+        this.name = name;
+    }
+    
+    public String getName(){
+        return name;
+    }
+    
+    public void setPassword(String password){
+        this.password = password;
+    }
+    
+    public String getPassword(){
+        return password;
+    }
+    
+    public void setVictoryCount(int victoryCount){
+        this.victoryCount = victoryCount;
+    }
+    
+    public int getVictoryCount(){
+        return victoryCount;
+    }
+    
+    public void setEmail(String email){
+        this.email = email;
+    }
+    
+    public String getEmail(){
+        return email;
+    }
 }
