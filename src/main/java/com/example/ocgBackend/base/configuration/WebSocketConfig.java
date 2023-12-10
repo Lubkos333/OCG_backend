c365afa16aea2f12d82b5b83061747f02396e8a4
lubos zeman <lubos.zeman@redrobot.cz>
2023-12-10 15:31:51 +0100

    WebSocket - config, controller, dependencies

---
+++ src/main/java/com/example/ocgBackend/base/configuration/WebSocketConfig.java
@@ -0,0 +1,31 @@
+package com.example.ocgBackend.base.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint(
+                "/endpoint-one",
+                        "/endpoint-two",
+                        "/endpoint-three",
+                        "/endpoint-four",
+                        "/endpoint-five")
+                .withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/game");
+        registry.setApplicationDestinationPrefixes("/game-get");
+
+    }
+
+}
---
+++ src/main/java/com/example/ocgBackend/webSocket/GameSocket.java
@@ -0,0 +1,16 @@
+package com.example.ocgBackend.webSocket;
+
+import com.example.ocgBackend.rest.api.dto.GameDto;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class GameSocket {
+
+    @MessageMapping("/send")
+    @SendTo("/result")
+    public GameDto gameMessaging(GameDto gameDto) {
+        return gameDto;
+    }
+}
