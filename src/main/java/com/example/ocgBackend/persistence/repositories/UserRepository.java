48cc794d6b428a61be9a549c12df375f73c1d56c
Lubkos333 <62471156+Lubkos333@users.noreply.github.com>
2023-12-11 17:12:14 +0100

    User one time sign in (#16)
    Co-authored-by: lubos zeman <lubos.zeman@redrobot.cz>

--- src/main/java/com/example/ocgBackend/persistence/repositories/UserRepository.java
+++ src/main/java/com/example/ocgBackend/persistence/repositories/UserRepository.java
@@ -4,6 +4,10 @@ import com.example.ocgBackend.persistence.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByName(String name);
 }
